5443e32ac120e3f42b5467e78bd6240bd5684250
Sitanshu Anmol <sitanshu.anmol@gmail.com>
2026-07-10 22:03:26 +0530

    indexes scripts

--- indexes/HNSW.py
+++ indexes/HNSW.py
@@ -3,4 +3,20 @@ import numpy as np
 from indexes.base import BaseIndex
 
 class HNSWIndex(BaseIndex):
-    #ANN search will have one param ef to know when to stop search (for constructor)
+    def __init__(self, dim: int, M: int = 32, metric_type=faiss.METRIC_L2):
+        super().__init__(dim)
+        self.index = faiss.IndexHNSWFlat(dim, M, metric_type)
+
+    def add(self, vectors: np.ndarray) -> None:
+        vectors = self._ensure_float32(vectors)
+        self.index.add(vectors)
+        self.ntotal = self.index.ntotal
+
+    def search(self, query_vectors: np.ndarray, k: int = 5):
+        query_vectors = self._ensure_float32(query_vectors)
+        distances, ids = self.index.search(query_vectors, k)
+        return distances, ids
+
+    def set_ef_search(self, ef_search: int):
+        self.ef_search = ef_search
+        self.index.hnsw.efSearch = ef_search
--- indexes/PQ.py
+++ indexes/PQ.py
@@ -1,3 +1,36 @@
 import faiss
-index = faiss.IndexPQ()
-# index.add()
+import numpy as np
+from indexes.base import BaseIndex
+
+class PQIndex(BaseIndex):
+    def __init__(self, dim: int, nlist: int, nbits: int, m):
+        super().__init__(dim)
+        self.nlist = nlist
+        self.m = m
+        # 8 or 16 since dim/m should be divisible
+        self.nbits = nbits
+        quantizer = faiss.IndexFlatL2(dim)
+        self.index = faiss.IndexIVFPQ(quantizer, dim, nlist, m, nbits)
+
+    def train(self, vectors: np.ndarray) -> None:
+        vectors = self._ensure_float32(vectors)
+        self.index.train(vectors)
+        self.is_trained = True
+
+    def add(self, vectors: np.ndarray) -> None:
+        if not self.is_trained:
+            raise RuntimeError("PQIndex must be trained before calling add()")
+
+        vectors = self._ensure_float32(vectors)
+        self.index.add(vectors)
+        self.ntotal = self.index.ntotal
+
+    def search(self, query_vectors: np.ndarray, k: int = 5):
+        query_vectors = self._ensure_float32(query_vectors)
+        distances, ids = self.index.search(query_vectors, k)
+        return distances, ids
+
+    def set_nprobe(self, nprobe: int) -> None:
+        self.nprobe = nprobe
+        self.index.nprobe = nprobe
+    
